2815dda082cc516a091ad49a5d7a5eacc5391f43
Katu_brd <katuthings@gmail.com>
2018-03-16 13:25:30 +0100

    basic sketch of slider mechanic

---
+++ vue-carousel/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container__g">
+    <carousel>
+      <carousel-slide>
+        Hi! here is the first one!
+        <img src="./assets/img/watermelon.jpg" />
+      </carousel-slide>
+      <carousel-slide>
+        Hi! here is the second one!
+        <img src="./assets/img/palms.jpg" />
+      </carousel-slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import Carousel from './components/carousel/carousel'
+import CarouselSlide from './components/carousel/carouselSlide'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  methods: {},
+  components: {
+    Carousel,
+    CarouselSlide
+  }
+}
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .container__g {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    display: grid;
+    justify-content: center;
+  }
+
+  img {
+    width: 100%;
+  }
+</style>
---
+++ vue-carousel/src/components/carousel/carousel.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <div class="carousel">
+    <slot></slot>
+    <button class="carousel__nav carousel__next" @click.prevent="next">next</button>
+    <button class="carousel__nav carousel__prev" @click.prevent="prev">prev</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        index: 0,
+        slides: []
+      }
+    },
+    mounted () {
+      this.slides = this.$children
+      this.slides.forEach((slide, i) => {
+        slide.index = i
+      })
+    },
+    computed: {
+      slidesCount () {
+        return this.slides.length
+      }
+    },
+    methods: {
+      next () {
+        this.index++
+        if (this.index >= this.slidesCount) {
+          this.index = 0
+        }
+      },
+      prev () {
+        this.index--
+        if(this.index < 0) {
+          this.index = this.slidesCount - 1
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+  .carousel {
+    width: 100%;
+    height: 100vh;
+    
+  }
+</style>
